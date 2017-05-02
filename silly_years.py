6e4bf460c5427d72a89956f1edc6ac9290cd31c3
Alec Johnson <alecjohnson55@gmail.com>
2017-05-01 20:17:32 -0700

    add problem statement

---
+++ silly_years.py
@@ -0,0 +1,7 @@
+# Write a function that takes a year (a 4 digit integer) and returns an array with the 10 closest subsequent years
+# that satisfy the following condition: The first two digits summed with the last two digits are equal to the
+# middle two digits
+
+# eg: 1978 => 19 + 78 = 97
+# eg: 2307 => 23 + 07 = 30
+
