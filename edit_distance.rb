e3fb2929ff9727e9c23605d914df503f1e192725
Alec Johnson <alecjohnson55@gmail.com>
2017-05-02 15:47:31 -0700

    add problem statement

---
+++ edit_distance.rb
@@ -0,0 +1,9 @@
+# Given two strings, str1 and str2, find the minimum number of edits (operations)
+# required to convert str1 to str2. You have the following actions: Insert, Remove, Replace.
+
+# examples:
+# str1 = 'geek', str2 = 'gesek', output = 1, because we insert an s.
+# str1  = 'cat', str2 = 'cut', output = 1, because we replace a with u.
+# str1 = 'sunday', str2 = 'saturday', output = 3, because we replace n
+# with r, and then insert t and a.
+
